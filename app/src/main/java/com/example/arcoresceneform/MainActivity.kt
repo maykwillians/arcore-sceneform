da45e7d3958033de63d5225fb2f94813bba353bb
Mayk Menezes <maykwillians@gmail.com>
2020-04-18 13:50:23 -0300

    renderizando objeto 3d

--- app/src/main/java/com/example/arcoresceneform/MainActivity.kt
+++ app/src/main/java/com/example/arcoresceneform/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.arcoresceneform
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var arFragment: ArFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        arFragment = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
+
+        arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
+            val anchor: Anchor = hitResult.createAnchor()
+
+            ModelRenderable.Builder()
+                .setSource(this, Uri.parse("ArcticFox_Posed.sfb"))
+                .build()
+                .thenAccept { modelRenderable: ModelRenderable? -> addModelToScene(anchor, modelRenderable!!) }
+        }
+    }
+
+    private fun addModelToScene(anchor: Anchor, modelRenderable: ModelRenderable) {
+        val anchorNode = AnchorNode(anchor)
+        val transformableNode = TransformableNode(arFragment.transformationSystem)
+        transformableNode.setParent(anchorNode)
+        transformableNode.renderable = modelRenderable
+        arFragment.arSceneView.scene.addChild(anchorNode)
+        transformableNode.select()
     }
 }
